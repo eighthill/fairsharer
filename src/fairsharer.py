78ae6b7a6ccc8e4e97ef50be6823cd3a755a1e83
eighthill <meric-2000@web.de>
2023-12-04 20:01:35 +0100

    added fairsharer code

--- src/fairsharer.py
+++ src/fairsharer.py
@@ -1,16 +1,23 @@
 def fair_sharer(values, num_iterations, share=0.1):
-	"""Runs num_iterations.
-	In each iteration the highest value in values gives a fraction (share)
-	to both the left and right neighbor. The leftmost field is considered
-	the neightbor of the rightmost field.
-	Examples:
-	fair_sharer([0, 1000, 800, 0], 1) --> [100, 800, 900, 0]
-	fair_sharer([0, 1000, 800, 0], 2) --> [100, 890, 720, 90]
-	Args
-	values:
-	1D array of values (list or numpy array)
-	num_iteration:
-	Integer to set the number of iterations
-	"""
-	# code
-	return values_new
+    values_new = values[:]  # Kopie der ursprünglichen Werte, um sie nicht zu verändern
+    
+    for _ in range(num_iterations):
+        max_index = values_new.index(max(values_new))  # Index des höchsten Wertes
+        max_value = values_new[max_index]  # Höchster Wert
+        
+        # Verteilung des Anteils des höchsten Wertes an die Nachbarn
+        left_neighbor = (max_index - 1) % len(values_new)  # Index des linken Nachbarn
+        right_neighbor = (max_index + 1) % len(values_new)  # Index des rechten Nachbarn
+        
+        # Berechnung der Anteile für die Nachbarn
+        share_to_give = max_value * share
+        values_new[left_neighbor] += share_to_give
+        values_new[right_neighbor] += share_to_give
+        
+        # Reduzierung des höchsten Wertes um den Anteil, der verteilt wurde
+        values_new[max_index] -= 2 * share_to_give
+    
+    return values_new
+
+fair_sharer([0, 1000, 800, 0], 1)  # --> [100, 800, 900, 0]
+fair_sharer([0, 1000, 800, 0], 2)  # --> [100, 890, 720, 90]
